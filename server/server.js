319b0aef5e07956ca548136ba1397200fd8a5291
mhd <m.hk.a1022@gmail.com>
2018-10-06 12:09:55 +0200

    Added delete todo by id route

--- server/server.js
+++ server/server.js
@@ -53,6 +53,25 @@ app.get("/todos/:id", (req, res) => {
   });
 });
 
+//Remove a single todo by id
+app.delete("/todos/:id", (req, res) => {
+  var id = req.params.id;
+
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send(todo);
+  }).catch((err) => {
+    res.status(400).send(err);
+  });
+});
+
 app.listen(Port, () => {
   console.log(`Http server running on localhost:${Port}`);
 });
