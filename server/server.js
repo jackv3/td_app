0f070414c2c1936f0db58701deffdb359eee8651
mhd <m.hk.a1022@gmail.com>
2018-10-06 10:24:42 +0200

    Setup app for heroku

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ var {mongoose} = require("./db/mongoose");
 var {Todo} = require("./models/todo");
 var {User} = require("./models/user");
 
-var Port = process.env.PORT || 3000;
+const Port = process.env.PORT || 3000;
 
 var app = express();
 //Express middleware
